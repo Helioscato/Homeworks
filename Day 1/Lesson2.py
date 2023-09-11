310e49f1509e90a004095537dab767673b2622a2
Helioscato <catohelios@gmail.com>
2023-09-11 20:40:49 +0400

    second commit

---
+++ Day 1/Lesson2.py
@@ -0,0 +1,35 @@
+#name = "dachi"
+#name არის ცვლადი
+# = არის ცვლადისთვის მნიშვნელობის მიმნიჭებელი სიმბოლო
+#"dachi" არის ცვლადისთვის მნიშვნელობა
+
+surname = "abashidze"
+
+#print(name)
+#პრინტ ფუნქციას გადაეცემა ეკრანზე გამოსატანი ობიექტში.
+
+name = "dachi" #ეს არის str (string) ტიპის ცვლადი
+age = 23 # ეს არის int (integer) მთელი რიცხვი
+height = 183.5 #ეს არის float ტყიპის ცვლადი (ათწილადი)
+# boolean (Bool) ტიპის ცვლადი
+
+knows_programming = True   #True ან False
+is_ugly = False
+
+
+
+
+#print(name + " " + surname)
+
+#print (name + age)
+
+
+#print(type(age))
+#print(type(name))
+#print(type(surname))
+#print(type(height))
+#print(type(knows_programming))
+
+print(name + " " + str(age))
+
+print("me var" + " " + (name) + " " + (surname)+ " " + "var" + " " + str(age) + " " + "wlis" + " " + "simaghle" + " " + str(height))
